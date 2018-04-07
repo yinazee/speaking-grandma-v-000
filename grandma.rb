da7d15fe70c93470ab27e17b3cbabb4dde34deca
Yina Zhu <yinagit@gmail.com>
2018-04-07 22:19:05 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -10,12 +10,11 @@
 # NO, NOT SINCE 1938!
 
 def speak_to_grandma(phrase)
-  if phrase != phrase.upcase
-    return "HUH?! SPEAK UP, SONNY!"
+  if phrase == phrase.upcase
+    return "NO, NOT SINCE 1938!"
   elsif phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-
-    else
-      return "NO, NOT SINCE 1938!"
+  else
+     return "HUH?! SPEAK UP, SONNY!"
   end
 end
