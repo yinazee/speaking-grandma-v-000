d891a20b8518a8f671a20640c66755949bc63ea7
Yina Zhu <yinagit@gmail.com>
2018-04-07 22:13:21 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -11,7 +11,7 @@
 
 def speak_to_grandma(phrase)
   if phrase == phrase.upcase
-    return "NO, NOT SINCE 1938!"
+    return "I LOVE YOU TOO PUMPKIN!"
   elsif phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
   else
