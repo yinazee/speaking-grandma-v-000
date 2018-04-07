d2fddf7124683e27a9a03163c16676989d575c67
Yina Zhu <yinagit@gmail.com>
2018-04-07 22:03:24 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -14,7 +14,6 @@ def speak_to_grandma(phrase)
     puts "NO, NOT SINCE 1938!"
   elsif phrase = "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
-
   else
     puts "HUH?! SPEAK UP, SONNY!"
 
